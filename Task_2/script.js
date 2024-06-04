30576c65fdfb93f0c54a641ccf4c1e2d9d4a316d
tanayshah2311 <tanay.shah@zeuslearning.com>
2024-06-04 17:59:14 +0530

    Task1-2 commit

---
+++ Task_2/script.js
@@ -0,0 +1,26 @@
+
+function validateform() {
+    var name = document.getElementById("yourname").value.trim();
+    var comments = document.getElementById("yourcomments").value.trim();
+    var male = document.getElementById("male");
+    var female = document.getElementById("female");
+
+
+  if (name === "") {
+    alert("All fields are compulsory");
+    document.getElementById("yourname").focus();
+    
+    } else if (comments === "") {
+    alert("All fields are compulsory");
+    document.getElementById("yourcomments").focus();
+    
+    } else if (!male.checked && !female.checked) {
+    alert("All fields are compulsory");
+   
+    }
+    else{
+    alert("Form Successfully filled");
+    }
+
+   
+}
